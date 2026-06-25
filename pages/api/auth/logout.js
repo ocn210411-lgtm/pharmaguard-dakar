ba4245263e21bf956aaeeae2f4d25fcc0f024536
ocn210411-lgtm <ocn210411.com>
2026-06-25 11:50:44 +0000

    Redirect to /admin/login after logout instead of /

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -3,5 +3,5 @@ import { getSession } from '../../../lib/session'
 export default async function handler(req, res) {
   const session = await getSession(req, res)
   session.destroy()
-  res.redirect('/')
+  res.redirect('/admin/login')
 }
